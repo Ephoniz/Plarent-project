cf44a1103ecd4afaeedf7d71aed6033f5c8624aa
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-08 12:42:00 +0200

    Planet created succesfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :planets, foreign_key: 'owner_id'
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
