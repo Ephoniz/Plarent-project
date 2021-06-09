a748309d75c125e62a2ac38e5f31348330cc7f52
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-09 17:02:08 +0200

    very last changes fro the first demo

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,33 +8,33 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+// External imports
+import "bootstrap";
+import Flickity from "flickity";
+
+
+
+// Internal imports, e.g:
+// import { initSelect2 } from '../components/init_select2';
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
+document.addEventListener('turbolinks:load', () => {
+  const elem = document.querySelector('.main-carousel');
+  const flkty = new Flickity( elem, {
+    // options
+    wrapAround: true,
+    groupCells: true,
+    freeScroll: true,
+    cellAlign: 'left'
+
+  });
+});
 
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
 // ----------------------------------------------------
-
-// External imports
-import "bootstrap";
-import Flickity from "flickity";
-
-const elem = document.querySelector('.main-carousel');
-const flkty = new Flickity( elem, {
-  // options
-  wrapAround: true,
-  groupCells: true,
-  freeScroll: true,
-  cellAlign: 'left'
-
-});
-
-// Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
-
-document.addEventListener('turbolinks:load', () => {
-  
-});
