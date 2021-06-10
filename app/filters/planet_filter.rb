3a531a22dc0d454fb557591660b0fba88d8ee2ee
Maruan Aguerdouh <maruan.mx@gmail.com>
2021-06-10 17:57:14 +0200

    Added rest of the fonts, a bit fo style and the rest of the filters

--- app/filters/planet_filter.rb
+++ app/filters/planet_filter.rb
@@ -1,16 +1,21 @@
 class PlanetFilter
-  attr_reader :galaxy, :radius
+  attr_reader :galaxy, :radius, :atmosphere, :temperature, :price
 
   def initialize(params)
     @galaxy = params.dig(:search, :galaxy)
     @radius = params.dig(:search, :radius)
+    @atmosphere = params.dig(:search, :atmosphere)
+    @temperature = params.dig(:search, :temperature)
+    @price = params.dig(:search, :price)
   end
 
   def filter!
     @planets = Planet.all
     filter_by_galaxy
     filter_by_radius
-    # ...
+    filter_by_price
+    filter_by_temperature
+    filter_by_atmosphere
 
     @planets
   end
@@ -27,5 +32,23 @@ class PlanetFilter
     return unless radius.present?
 
     @planets = @planets.where('radius <= ?', radius)
+  end
+
+  def filter_by_temperature
+    return unless temperature.present?
+
+    @planets = @planets.where('temperature <= ?', temperature)
+  end
+
+  def filter_by_price
+    return unless price.present?
+
+    @planets = @planets.where('price <= ?', price)
+  end
+
+  def filter_by_atmosphere
+    return unless atmosphere.present?
+
+    @planets = @planets.where(atmosphere: atmosphere)
   end
 end
